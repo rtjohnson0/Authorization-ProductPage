edc9dc18ae28e211373e6b124bbaaae343cb6180
Reggie Johnson <52217056+rtjohnson0@users.noreply.github.com>
2019-12-02 09:14:26 -0500

    update home

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -50,12 +50,7 @@ const Home = (props) => {
                         </div>
 
 
-                        <a class="carousel-control-prev" href="#demo" data-slide="prev">
-                            <span class="carousel-control-prev-icon"></span>
-                        </a>
-                        <a class="carousel-control-next" href="#demo" data-slide="next">
-                            <span class="carousel-control-next-icon"></span>
-                        </a>
+                   
                     </main>
                     <div class="col-md-12 p-lg-5 mx-auto my-5">
                         <div id="demo" class="carousel slide" data-ride="carousel">
@@ -65,7 +60,7 @@ const Home = (props) => {
                                 <p class="lead font-weight-normal col-md-12">With over 6,100 stores located throughout the United States and 17 countries, we are the retail destination for gamers around the world. Our GameStop, EB Games and Electronics Boutique retail locations set us apart in the industry.
                                     Everything that we offer our customers-from our expansive selection of new products, to our knowledgeable associates and our value-added pre-owned products-is geared to deliver customer satisfaction. </p>
                             </div>
-                            <a class="btn btn-outline-success" href="/">Coming soon</a>
+                           
                         </div>
                     </div>
                     <div class="product-device shadow-sm d-none d-md-block"></div>
@@ -78,39 +73,17 @@ const Home = (props) => {
                     <div class="col-md-5 p-lg-5 mx-auto my-5">
 
                         <div class="white">
-                            <h1 class="display-4 font-weight-normal">Upcoming Games</h1>
+                            <h1 class="display-4 font-weight-normal text-white">Upcoming Games</h1>
                         </div>
-                        <div id="slideshow">
-                            <div>
-                                <img src="https://i0.wp.com/www.comicsbeat.com/wp-content/uploads/2019/05/title.jpg?fit=1600%2C800&ssl=1" height="528" width="920" alt="Marvel Allegience 3" /><br />
-                                <div class="white"> Marvel Allegience 3 (Nintendo Switch)</div>
-                            </div>
-                            <div>
-                                <img src="https://news.xbox.com/en-us/wp-content/uploads/sites/2/2019/07/NBA2k20_Legend_KeyArt_940x528.jpg?fit=940%2C528" alt="NBA 2K" /><br />
-                                <div class="white"> NBA 2K20 (All Platforms)</div>
-                            </div>
-                            <div>
-                                <img src="https://irnpost.com/wp-content/uploads/2019/05/MADUSE20_ADHOC_Main_940x528.jpg " alt="Madden 20 " /><br />
-                                <div> Madden NFL 20 (All Platforms)</div>
-                            </div>
-                            <div>
-                                <img src="https://realsport101.com/wp-content/uploads/2018/10/Ronaldo-fifa-19-24-1024x576.png" height="528" alt="Fifa 20" /><br /> Fifa 20 (All Platforms)
-                                </div>
-                            <div>
-                                <img src="https://i0.wp.com/metro.co.uk/wp-content/uploads/2019/05/Screenshot_4-46c3.jpg?quality=90&strip=all&zoom=1&resize=644%2C330&ssl=1 " height="528" width="920" alt="Modern Warfare " /><br /> Modern Warfare (All Platforms) race
-                                car.
-                                </div>
-                        </div>
+                 
 
                         <hr />
 
 
                         <div class="white">
-                            <p class="lead font-weight-normal ">Feel free to come in or call to put your name on the list for the first release! Call 740-555-5555 to get your game today!.</p>
+                            <p class="lead font-weight-normal text-white ">Feel free to come in or call to put your name on the list for the first release! Call 740-555-5555 to get your game today!.</p>
                         </div>
-                        <a class="btn btn-outline-success " href="# ">
-                            <div class="big ">Coming soon</div>
-                        </a>
+                       
                     </div>
                     <div class="product-device shadow-sm d-none d-md-block "></div>
                     <div class="product-device product-device-2 shadow-sm d-none d-md-block "></div>
